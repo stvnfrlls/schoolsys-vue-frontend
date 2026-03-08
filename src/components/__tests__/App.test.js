4d46af7ff3648f56e3c949ff8ce3ecd08b814688
Steven Frilles <stvnfrlls@gmail.com>
2026-03-08 12:33:28 +0800

    fix package.json script

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
-import App from '../App.vue'  // adjust path to an actual component
+import App from '../../App.vue'  // adjust path to an actual component
 
 describe('App', () => {
     it('mounts without crashing', () => {
